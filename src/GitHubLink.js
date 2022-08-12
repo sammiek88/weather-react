30ecd94a8931696c190bab587862d0031424e124
sammiek88 <67045522+sammiek88@users.noreply.github.com>
2022-08-12 15:43:23 +0100

    Add github link

---
+++ src/GitHubLink.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export default function gitHub() {
+  return (
+    <div>
+      <a href="https://github.com/sammiek88">
+        <img src="https://github.com/logos" target="_blank" alt="github" />
+      </a>
+    </div>
+  );
+}
