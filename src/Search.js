bd63061a197030346984577b854ab20f0e72efd0
sammiek88 <67045522+sammiek88@users.noreply.github.com>
2022-08-12 15:19:08 +0100

    Rounded temp and upper case description

--- src/Search.js
+++ src/Search.js
@@ -1,33 +1,11 @@
 import React, { useState } from 'react';
+// eslint-disable-next-line
 import axios from 'axios';
 
 export default function Search() {
   const [loaded, setLoaded] = useState(false);
   const [city, setCity] = useState('');
-  const [weather, setWeather] = useState('');
-
-  let form = (
-    <form onSubmit={handleSubmit}>
-      <input
-        type="search"
-        placeholder="Enter a city..."
-        onChange={updateCity}
-      ></input>
-      <input type="submit" value="Search"></input>
-    </form>
-  );
-
-  function handleSubmit(event) {
-    let apiKey = '094780c710fa4efd669f0df8c3991927';
-    let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
-
-    event.preventDefault();
-    axios.get(apiUrl).then(displayWeather);
-  }
-
-  function updateCity(event) {
-    setCity(event.target.value);
-  }
+  const [weather, setWeather] = useState({});
 
   function displayWeather(response) {
     setLoaded(true);
@@ -40,14 +18,37 @@ export default function Search() {
     });
   }
 
+  function handleSubmit(event) {
+    event.preventDefault();
+    let apiKey = '094780c710fa4efd669f0df8c3991927';
+    let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+
+    axios.get(apiUrl).then(displayWeather);
+  }
+
+  function updateCity(event) {
+    setCity(event.target.value);
+  }
+
+  let form = (
+    <form onSubmit={handleSubmit}>
+      <input
+        type="search"
+        placeholder="Enter a city..."
+        onChange={updateCity}
+      ></input>
+      <input type="submit" value="Search"></input>
+    </form>
+  );
+
   if (loaded) {
     return (
       <div>
         {form}
         <ul className="forcast">
-          <li>Temperature: {Math.round(weather.temp)}</li>
-          <li>Wind: {Math.round(weather.wind)}</li>
-          <li>Humidity: {weather.humidity}</li>
+          <li>Temperature: {Math.round(weather.temp)}°C</li>
+          <li>Wind: {Math.round(weather.wind)}km/h</li>
+          <li>Humidity: {weather.humidity}%</li>
           <li>{weather.description.toUpperCase()}</li>
           <li>
             <img alt="weathericon" src={weather.icon} />
