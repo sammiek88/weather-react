ff5308054de6de996c271b996ac735db6ecd46b0
sammiek88 <67045522+sammiek88@users.noreply.github.com>
2022-08-12 13:57:22 +0100

    Display temperature of the selected city in a H2

--- src/Search.js
+++ src/Search.js
@@ -3,24 +3,37 @@ import axios from 'axios';
 
 export default function Search() {
   const [city, setCity] = useState('');
+  const [temp, setTemp] = useState('');
 
   function handleSubmit(event) {
+    let apiKey = '094780c710fa4efd669f0df8c3991927';
+    let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+
     event.preventDefault();
-    alert(city);
+
+    axios.get(apiUrl).then(displayWeather);
   }
 
   function updateCity(event) {
     setCity(event.target.value);
   }
 
+  function displayWeather(response) {
+    setTemp(response.data.main.temp);
+  }
+
   return (
-    <form onSubmit={handleSubmit}>
-      <input
-        type="search"
-        placeholder="Enter a city..."
-        onChange={updateCity}
-      ></input>
-      <input type="submit" value="Search"></input>
-    </form>
+    <div>
+      <form onSubmit={handleSubmit}>
+        <input
+          type="search"
+          placeholder="Enter a city..."
+          onChange={updateCity}
+        ></input>
+        <input type="submit" value="Search"></input>
+      </form>
+
+      <h2>{temp}</h2>
+    </div>
   );
 }
