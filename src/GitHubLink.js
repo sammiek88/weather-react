d058723df76adb6ef968087ab2f292f0a1645c71
sammiek88 <67045522+sammiek88@users.noreply.github.com>
2022-08-12 15:56:05 +0100

    Update Github link

--- src/GitHubLink.js
+++ src/GitHubLink.js
@@ -4,10 +4,7 @@ export default function gitHub() {
   return (
     <div>
       <a href="https://github.com/sammiek88" target="_blank" rel="noreferrer">
-        <img
-          src="GitHub-Mark/PNG/GitHub-Mark-Light-120px-plus.png"
-          alt="github"
-        />
+        Find me on GitHub
       </a>
     </div>
   );
