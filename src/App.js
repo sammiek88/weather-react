677415063cbcd16b951f300130960d59797c7d26
sammiek88 <67045522+sammiek88@users.noreply.github.com>
2022-08-25 15:46:44 +0100

    Fix date and time

--- src/App.js
+++ src/App.js
@@ -1,14 +1,12 @@
 import React from 'react';
 import GitHubLink from './GitHubLink.js';
 import Search from './Search';
-import CurrentDate from './CurrentDate';
 
 import './App.css';
 
 export default function App() {
   return (
     <div className="container">
-      <CurrentDate />
       <Search />
       <GitHubLink />
     </div>
