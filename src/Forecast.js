896b0498fb883280cf73cf0666912a2f81055648
sammiek88 <67045522+sammiek88@users.noreply.github.com>
2022-09-06 20:22:46 +0100

    edited layout with bootstrap

--- src/Forecast.js
+++ src/Forecast.js
@@ -5,74 +5,76 @@ import './Forecast.css';
 export default function () {
   return (
     <div className="ForecastTable p-2">
-      <div className="row">
-        <div className="col-2">
-          Mon
-          <div>
-            <img
-              classname="icon"
-              src="https://ssl.gstatic.com/onebox/weather/64/partly_cloudy.png"
-            ></img>
-            <div className="dailyWeather">
-              <span>17 |</span>
+      <div class="container text-center">
+        <div class="row row-cols-auto">
+          <div class="col">
+            Mon
+            <div>
+              <img
+                classname="icon"
+                src="https://ssl.gstatic.com/onebox/weather/64/partly_cloudy.png"
+              ></img>
+              <div className="dailyWeather">
+                <span>17 |</span>
 
-              <span> 20</span>
+                <span> 20</span>
+              </div>
             </div>
           </div>
-        </div>
-        <div className="col-2">
-          Tues
-          <div>
-            <img
-              classname="icon"
-              src="https://ssl.gstatic.com/onebox/weather/64/partly_cloudy.png"
-            ></img>
-            <div className="dailyWeather">
-              <span>17 |</span>
+          <div class="col">
+            Tues
+            <div>
+              <img
+                classname="icon"
+                src="https://ssl.gstatic.com/onebox/weather/64/partly_cloudy.png"
+              ></img>
+              <div className="dailyWeather">
+                <span>17 |</span>
 
-              <span> 20</span>
+                <span> 20</span>
+              </div>
             </div>
           </div>
-        </div>
-        <div className="col-2">
-          Wed
-          <div>
-            <img
-              classname="icon"
-              src="https://ssl.gstatic.com/onebox/weather/64/partly_cloudy.png"
-            ></img>
-            <div className="dailyWeather">
-              <span>17 |</span>
+          <div class="col">
+            Wed
+            <div>
+              <img
+                classname="icon"
+                src="https://ssl.gstatic.com/onebox/weather/64/partly_cloudy.png"
+              ></img>
+              <div className="dailyWeather">
+                <span>17 |</span>
 
-              <span> 20</span>
+                <span> 20</span>
+              </div>
             </div>
           </div>
-        </div>
-        <div className="col-2">
-          Thurs
-          <div>
-            <img
-              classname="icon"
-              src="https://ssl.gstatic.com/onebox/weather/64/partly_cloudy.png"
-            ></img>
-            <div className="dailyWeather">
-              <span>17 |</span>
+          <div class="col">
+            Thurs
+            <div>
+              <img
+                classname="icon"
+                src="https://ssl.gstatic.com/onebox/weather/64/partly_cloudy.png"
+              ></img>
+              <div className="dailyWeather">
+                <span>17 |</span>
 
-              <span> 20</span>
+                <span> 20</span>
+              </div>
             </div>
           </div>
-        </div>
-        <div className="col-2">
-          Fri
-          <div>
-            <img
-              classname="icon"
-              src="https://ssl.gstatic.com/onebox/weather/64/partly_cloudy.png"
-            ></img>
-            <div className="dailyWeather">
-              <span>17 |</span>
+          <div class="col">
+            Fri
+            <div>
+              <img
+                classname="icon"
+                src="https://ssl.gstatic.com/onebox/weather/64/partly_cloudy.png"
+              ></img>
+              <div className="dailyWeather">
+                <span>17 |</span>
 
-              <span> 20</span>
+                <span> 20</span>
+              </div>
             </div>
           </div>
         </div>
